29b41878c5c7f2ec4e843baea5b3c30f50bf0d9c
matronae <mykaomas@gmail.com>
2024-02-20 16:10:41 -0600

    started code for server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require('./config');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set Handlebars as the default template engine.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+sequelize.sync().then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  }).catch(err => {
+    console.error('Error syncing database:', err);
+  });
