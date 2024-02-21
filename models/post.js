90a260de92e1adfeb4b39c0c6b347ef6bbf79cd1
matronae <mykaomas@gmail.com>
2024-02-20 18:28:50 -0600

    created post model

---
+++ models/post.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config');
+
+const Post = sequelize.define('Post', {
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    content: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+});
+
+module.exports = Post;
